6dfcdbc6705aee1579c2869d0ac89562b6cc0f67
Shehan R.K <ruwanbrother1@gmail.com>
2025-07-19 07:30:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        fallback: 'index.html', // add this
+      },
+    },
   },
-});
+})
