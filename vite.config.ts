c6236e1007aa24243955b9a2d59f361116b56bcc
Shehan R.K <ruwanbrother1@gmail.com>
2025-07-19 16:44:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NexonMACS/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        fallback: 'index.html', // add this
-      },
-    },
+  base:'/NexonMACS/',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
-})
+});
