1f99187d6df8be0df7eec6a59ce90117fd8b4d26
Shehan R.K <ruwanbrother1@gmail.com>
2025-07-19 08:56:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import LandingPage from './components/LandingPage';
 import StudentProfile from './components/student/StudentProfile';
